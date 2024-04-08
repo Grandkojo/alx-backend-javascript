38a5faf5f9efda55cd0be047556ea51f6bb0a99b
Grandkojo <essienernest.kojoowusu@gmail.com>
2024-04-08 22:23:01 +0000

    ES6-basics

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+    let newarray = [];
+    for (let idx of array) {
+      newarray.push(appendString + idx);
+    }
+  
+    return newarray;
+  }
