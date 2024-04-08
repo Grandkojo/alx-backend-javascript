5f14db5ce6e511453069a2f7e055bcf2fb26602c
Grandkojo <essienernest.kojoowusu@gmail.com>
2024-04-08 21:31:50 +0000

    Arrow

---
+++ 0x00-ES6_basic/2-main.js
@@ -0,0 +1,5 @@
+import getNeighborhoodsList from "./2-arrow.js";
+
+const newNeighborhoodsList = getNeighborhoodsList();
+const res = newNeighborhoodsList.addNeighborhood("Noe Valeey");
+console.log(res);
