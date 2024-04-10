553fb4eb5205ceadc193ae46578e5782d9a6c3f6
Grandkojo <essienernest.kojoowusu@gmail.com>
2024-04-10 23:07:43 +0000

    ES6_promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,19 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(
+  firstname,
+  lastname,
+  filename,
+) {
+  const signUpPromise = await signUpUser(firstname, lastname);
+  const uploadPromise = await uploadPhoto(filename);
+
+  const result = await Promise.allSettled([signUpPromise, uploadPromise]);
+  console.log(result);
+
+  return result.map(({ status, value, reason }) => ({
+    status,
+    value: status === 'fulfilled' ? value : reason,
+  }));
+}
