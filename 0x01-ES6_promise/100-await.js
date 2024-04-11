9bbb85706cd9e645cc59b8b60e5a200bba8fa813
Grandkojo <essienernest.kojoowusu@gmail.com>
2024-04-11 00:23:23 +0000

    ES6_promises

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,16 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  const user = await createUser();
+  const photo = await uploadPhoto();
+  if (user && photo) {
+    return {
+      photo,
+      user,
+    };
+  }
+  return {
+    photo: null,
+    user: null,
+  };
+}
