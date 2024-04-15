d40c51d631c932ff58dd43535bb745704c6e804f
Grandkojo <essienernest.kojoowusu@gmail.com>
2024-04-15 23:45:57 +0000

    ES6-data-manipulations

---
+++ 0x03-ES6_data_manipulation/1-main.js
@@ -0,0 +1,5 @@
+import getListStudentIds from './1-get_list_student_ids';
+import getListStudent from './0-get_list_students';
+
+console.log(getListStudentIds('hello'));
+console.log(getListStudentIds(getListStudent()));
