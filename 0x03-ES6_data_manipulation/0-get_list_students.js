fc1e0f670bc1abe3f78b027c1f8de60cd8647f2d
Grandkojo <essienernest.kojoowusu@gmail.com>
2024-04-15 23:26:14 +0000

    ES6-data-manipulations

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,9 @@
 export default function getListStudents() {
   const arr = [];
-  arr.push({ id: 1, name: 'Guillaume', location: 'San Francisco' });
+  arr.push({ id: 1, FirstName: 'Guillaume', location: 'San Francisco' });
 
-  arr.push({ id: 2, name: 'James', location: 'Columbia' });
+  arr.push({ id: 2, FirstName: 'James', location: 'Columbia' });
 
-  arr.push({ id: 5, name: 'Serena', location: 'San Francisco' });
+  arr.push({ id: 5, FirstName: 'Serena', location: 'San Francisco' });
   return arr;
 }
