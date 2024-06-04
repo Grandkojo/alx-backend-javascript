1d757839f2061ca96560dd6041cfabbd90c8bf27
Grandkojo <essienernest.kojoowus@gmail.com>
2024-06-04 16:33:46 +0000

    using process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,15 @@
+const proc = require('process');
+
+const app = ((proc) => {
+	proc.stdin.setEncoding('utf-8');
+	proc.stdout.write('Welcome to Holberton School, what is your name?\n');
+	proc.stdin.on('readable', () => {
+		const chunk = proc.stdin.read();
+		if (chunk) { proc.stdout.write(`Your name is: ${chunk}`);}
+	});
+	proc.stdin.on('end', () => {
+		proc.stdout.write('This important software is now closing\n');
+	});
+})(proc);
+
+module.exports = app;
